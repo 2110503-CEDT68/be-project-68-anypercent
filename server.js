a627e154f4c2315a2dab914eea4eb6de60e11110
Phao Phao <phaophao@Phaos-MacBook-Air-2.local>
2026-02-24 13:32:41 +0700

    assignment 7

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -13,8 +14,16 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 const hospitals = require ('./routes/hospitals');
+const auth = require('./routes/auth');
+const appointments =require('./routes/appointments');
+
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments);
 
 const PORT = process.env.PORT || 5003;
 
